d0606fa115e301dd3a9e39106bd5095e467598a9
lbenavides23 <leopoldo.benavides@gmail.com>
2018-07-12 05:11:29 +0000

    comment out section

--- src/javascript/preselect.js
+++ src/javascript/preselect.js
@@ -1,43 +1,43 @@
-import $ from 'jquery';
+// import $ from 'jquery';
 
 
-$(document).ready(function(){
+// $(document).ready(function(){
 
-    window.onload = function(){ 
-      // Get the modal
-    var modal = document.getElementById('selectModal');
+//     window.onload = function(){ 
+//       // Get the modal
+//     var modal = document.getElementById('selectModal');
     
-    // Get the button that opens the modal
-    var psBtn = document.getElementById("myBtn");
+//     // Get the button that opens the modal
+//     var psBtn = document.getElementById("myBtn");
     
-    // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[0];
+//     // Get the <span> element that closes the modal
+//     var span = document.getElementsByClassName("close")[0];
     
-    // When the user clicks the button, open the modal 
-    psBtn.onclick = function() {
-        modal.style.display = "block";
-    }
+//     // When the user clicks the button, open the modal 
+//     psBtn.onclick = function() {
+//         modal.style.display = "block";
+//     }
     
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() {
-        modal.style.display = "none";
-    }
+//     // When the user clicks on <span> (x), close the modal
+//     span.onclick = function() {
+//         modal.style.display = "none";
+//     }
     
     
-    var kep = document.getElementById("btn-kp");
+//     var kep = document.getElementById("btn-kp");
     
-    kep.onclick = function() {
-        modal.style.display = "none";
-    }
+//     kep.onclick = function() {
+//         modal.style.display = "none";
+//     }
     
-    // When the user clicks anywhere outside of the modal, close it
-    window.onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
-        }
-    }
+//     // When the user clicks anywhere outside of the modal, close it
+//     window.onclick = function(event) {
+//         if (event.target == modal) {
+//             modal.style.display = "none";
+//         }
+//     }
     
-    };
+//     };
 
   
-});
+// });
